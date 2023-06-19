8ce60ab571997600eff354c6b749e52f9588b433
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-06-19 15:14:39 +0200

    update

--- algo_python/Correction/dice_warriors/caracter.py
+++ algo_python/Correction/dice_warriors/caracter.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dice import Dice, RiggedDice
+from rich import print
 
 
 class Caracter:
@@ -23,6 +24,12 @@ class Caracter:
     def get_name(self):
         return self.name
     
+    def get_defense_value(self):
+        return self.defense_value
+    
+    def regenerate(self):
+        self.health = self.max_health
+    
     def is_alive(self):
         return self.health > 0
     
@@ -35,29 +42,54 @@ class Caracter:
         if (self.health < 0):
             self.health = 0
         
+    def compute_attack(self, result, target):
+        return self.attack_value + result
+        
     def attack(self, target: Caracter) -> None:
         if (self.is_alive()):
             result = self.dice.roll()
-            damages = self.attack_value + result
+            damages = self.compute_attack(result, target)
             print(f"⚔️  {self.get_label()} {self.name} attack {target.get_label()} {target.get_name()} with {damages} damages (att: {self.attack_value} + roll: {result}) !")
             target.defend(damages, self)
     
+    def compute_defend(self, damages, result):
+        return damages - self.defense_value - result
+    
     def defend(self, damages: int, attacker: Caracter):
         result = self.dice.roll()
-        wounds = damages - self.defense_value - result
+        wounds = self.compute_defend(damages, result)
         print(f"🛡️ {self.get_label()} {self.name} defend against {attacker.get_label()} {attacker.get_name()} for {damages} damages and take {wounds} wounds (dmg: {damages} - def: {self.defense_value} - roll: {result}) !")
         self.decrease_health(wounds)
         self.show_healthbar()
 
+
 class Warrior(Caracter):
-    label = "Warrior"
+    label = "[red]Warrior[/red]"
+    
+    def compute_attack(self, result, target):
+        print("🪓 Axe your in face ! (att bonus: +3)")
+        return super().compute_attack(result, target) + 3
+    
 
 class Mage(Caracter):
-    label = "Mage"
+    label = "[blue]Mage[/blue]"
+    
+    def compute_defend(self, damages, result):
+        print("🔮 Magic armor (def bonus: -3)")
+        return super().compute_defend(damages, result) - 3
+    
+    
+class Thief(Caracter):
+    label = "[green]Thief[/green]"
+    
+    def compute_attack(self, result, target: Caracter):
+        print(f"🔪 Backstab ! (att bonus: + {target.get_defense_value()})")
+        return super().compute_attack(result, target) + target.get_defense_value()
+
 
 if __name__ == "__main__":
     car1 = Warrior("James", 20, 8, 3, Dice())
-    car2 = Mage("Elise", 20, 8, 3, Dice())
+    car2 = Thief("Elise", 20, 8, 3, Dice())
     
     print(car1)
     print(car2)
