85f4a835ecb8a56b1578095fcc20df5f78687a98
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-06-19 10:06:36 +0200

    update

--- algo_python/Correction/dice_warriors/caracter.py
+++ algo_python/Correction/dice_warriors/caracter.py
@@ -5,12 +5,20 @@ from dice import Dice, RiggedDice
 class Caracter:
     label = "Caracter"
     
-    def __init__(self):
-        pass
+    def __init__(self, name, max_health = 20, attack = 8, defense = 3):
+        self.name = name
+        self.max_health = max_health
+        self.attack_value = attack
+        self.defense_value = defense
     
     def __str__(self):
-        return f"I'm a {type(self).label}"
+        return f"I'm {self.name} the {type(self).label} ({self.max_health}hp, att: {self.attack_value}, def: {self.defense_value})"
 
+
+class Warrior(Caracter):
+    label = "Warrior"
 
 if __name__ == "__main__":
     a_dice = Dice()
+    car1 = Warrior("James", 20, 8, 3)
+    print(car1)
