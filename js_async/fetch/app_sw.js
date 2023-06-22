cdd6824344b3171e58eea2b41e699df0ab083306
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-06-22 14:45:24 +0200

    update

--- js_async/fetch/app_sw.js
+++ js_async/fetch/app_sw.js
@@ -1,2 +1,27 @@
 // m'affichiez dans la console
-    // la planète d'origine des personnages du films n°2
+// la planète d'origine des personnages du films n°2
+
+fetch("https://swapi.dev/api/films/2/")
+    .then(response => response.json())
+    .then(carUrlList => {
+        let caracterPromises = []
+        carUrlList.characters.forEach(carUrl => {
+            let promise = fetch(carUrl)
+                .then(response => response.json())
+                .then(car => {
+                    return fetch(car.homeworld)
+                        .then(response => response.json())
+                        .then(planet => {
+                            return {name: car.name, homeworld: planet.name}
+                        })
+                })
+            caracterPromises.push(promise)
+        });
+        console.log(caracterPromises);
+        return Promise.all(caracterPromises)
+    })
+    .then((caracters) => {
+        for (let car of caracters) {
+            console.log(`${car.name} -> ${car.homeworld}`);
+        }
+    })
