e8db5fb15b0a4c26e7171adfaf443b77377c54a3
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-06-23 08:22:41 +0200

    update

--- js_async/async/app_sw.js
+++ js_async/async/app_sw.js
@@ -1,18 +1,34 @@
-async function fetchDatas() {
+async function fecthDatasToJSON(url) {
 
-    const filmResponse = await fetch("https://swapi.dev/api/films/2/")
-    const film = await filmResponse.json()
+    // const response = await fetch(url)
+    // const json = await response.json()
+    // return json
+
+    // Equivalent (refactorisé)
+
+    return await fetch(url).then(r => r.json())
+}
+
+async function getSWAPIDatas() {
+
+    // const filmResponse = await fetch("https://swapi.dev/api/films/2/")
+    // const film = await filmResponse.json()
+
+    const film = await fecthDatasToJSON("https://swapi.dev/api/films/2/")
 
     for (let carURL of film.characters) {
-        const charReponse = await fetch(carURL)
-        // fetch
-        const car = await charReponse.json()
+        // const charReponse = await fetch(carURL)
+        // const car = await charReponse.json()
 
-        const homeResponse = await fetch(car.homeworld)
-        const home = await homeResponse.json()
+        const car = await fecthDatasToJSON(carURL)
+
+        // const homeResponse = await fetch(car.homeworld)
+        // const home = await homeResponse.json()
+
+        const home = await fecthDatasToJSON(car.homeworld)
 
         console.log(`${car.name} -> ${home.name}`);
     }
 }
 
-fetchDatas();
+getSWAPIDatas();
