60d461d6ff4820c619d7020e48c668363996f93f
Gladrat <3368584+Gladrat@users.noreply.github.com>
2023-07-03 11:08:41 +0200

    update

---
+++ nodejs/api_rest/api_todo.js
@@ -0,0 +1,4 @@
+// Asynchrone !
+// 1. Lire le contenu du fichier JSON dans une fonction
+// 2. Proposer un endpoint GET sur la racine "/" d'un webserver (127.0.0.1:8000)
+    // Qui charge le contenu du fichier JSON
